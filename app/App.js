e16fab8486a0e5552085cd09e33c7c56d57725b7
Billie Zhang <billiecoder@gmail.com>
2015-08-29 14:41:55 +0800

    i

--- app/App.js
+++ app/App.js
@@ -1,22 +1,35 @@
-import React from 'react';
+var React = require('react');
+require("./main.css");
 
-class App extends React.Component{
-  getInitialState() {
+var Box = React.createClass({
+  getInitialState: function() {
     return {
-      names: ["billie"]
+      names: ['billie66', 'peter', '', '']
     };
   },
 
-  render() {
+  render: function() {
+    var names = this.state.names.map(function(name, index) {
+      if (name !== '') {
+        var image = 'http://media.haoduoshipin.com/modern/avatar/' + name + '.jpg';
+        return (
+            <li className="profile" key={index}>
+              <img src={image} />
+            </li>
+          );
+      } else {
+        return (<li className="profile" key={index}></li>);
+      }
+    });
+
     return (
-      <div className="profiles-container">
-        <ul>
-          {this.state.names}
+      <div className="profiles-wrapper">
+        <ul className="clearfix">
+          {names}
         </ul>
-
       </div>
-    )
+    );
   }
-};
+});
 
-React.render(<App />, document.getElementById('app'));
+React.render(<Box />, document.getElementById('app'));
